ae3b4b3d140f35307a1c167bf439e9d9e4c73cb9
Jeroen van der Borgh <jeroen.vanderborgh@gmail.com>
2021-01-07 12:45:28 +0100

    load_board

--- code/classes/board.py
+++ code/classes/board.py
@@ -1,11 +1,24 @@
+import numpy as np
+
 class Board():
     pass
 
-    def __init__(self):
-        pass
+    def __init__(self, size, csv):
+        self.size = size
+        self.cars = {}
+        #load_cars(csv)
+        self.board = np.full((size, size), "#")
+        load_board(self)
 
     def load_board(self):
-        pass
+        # fill the board
+        for car in self.cars.values():
+            for i in range(car.length):
+                if car.orientation == "H":
+                    self.board[car.y][car.x + i] = car.letter_id
+                else:
+                    self.board[car.y + i][car.x] = car.letter_id
+
 
     def load_cars(self, source_file):
         pass
