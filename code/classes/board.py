5fa87b0b3edd21da13541db90ce7cb918ccdceac
Jeroen van der Borgh <jeroen.vanderborgh@gmail.com>
2021-01-11 14:53:26 +0100

    added collormaps in the animation

--- code/classes/board.py
+++ code/classes/board.py
@@ -171,15 +171,62 @@ class Board():
                 car.moves = (-moves_forward, moves_backward)
             
 def make_animation(moves, size, csv):
-    board = Board(6,csv)
-    colormap = { "X" : [255,0,0] , "A" : [0,0,255], "B" : [255,255,0],
-            "C" : [171, 106, 30], "D" : [40, 96, 99], "E" : [0,0,255],
-            "F" : [40, 96, 99],"G" : [0,255,255],"H" : [0,0,255],
-            "I" : [0,255,255],"J" : [40, 96, 99],"K" : [171, 106, 30],"L" : [40, 96, 99]}
-    
+    board = Board(size,csv)
+    red = [255,0,0]
+    blue = [0,0,255]
+    yellow = [255, 255, 0]
+    dark_yellow = [150, 150, 0]
+    orange = [171, 106, 30]
+    light_blue = [0,255,255]
+    dark_blue = [40, 96, 99]
+    purple = [255, 0, 255]
+    grey = [120, 120, 120]
+
+    colormaps = {
+        "./data/gameboards/Rushhour6x6_1.csv" : { "X" : red, "A" : blue, "B" : yellow,
+                "C" : orange, "D" : dark_blue, "E" : blue,
+                "F" : dark_blue,"G" : light_blue,"H" : blue,
+                "I" : light_blue,"J" :dark_blue,"K" : orange,"L" : dark_blue},
+        
+        "./data/gameboards/Rushhour6x6_2.csv" : { "X" : red, "A" : blue, "B" : orange,
+                "C" : orange, "D" : dark_blue, "E" : dark_blue,
+                "F" : blue,"G" : light_blue,"H" : yellow,
+                "I" : dark_blue,"J" : orange,"K" : light_blue,"L" : orange},
+
+        "./data/gameboards/Rushhour6x6_3.csv" : { "X" : red, "A" : light_blue, "B" : purple,
+                "C" : yellow, "D" : orange, "E" : light_blue,
+                "F" : orange,"G" : orange,"H" : dark_blue},
+
+        "./data/gameboards/Rushhour9x9_4.csv" : { "X" : red, "A" : yellow, "B" : dark_blue,
+                "C" : purple, "D" : grey, "E" : blue,
+                "F" : purple, "G" : dark_yellow, "H" : yellow,
+                "I" : light_blue, "J" : dark_blue, "K" : purple, "L" : orange,
+                "M" : dark_blue, "N" : yellow, "O" : blue, "P" : dark_yellow,
+                "Q" : blue, "R" : grey, "S" : orange, "T" : light_blue,
+                "U" : dark_blue},
+
+        "./data/gameboards/Rushhour9x9_5.csv" : { "X" : red, "A" : yellow, "B" : dark_blue,
+                "C" : blue, "D" : dark_blue, "E" : purple,
+                "F" : orange, "G" : blue, "H" : light_blue,
+                "I" : orange, "J" : dark_yellow, "K" : blue, "L" : orange,
+                "M" : dark_blue, "N" : purple, "O" : blue, "P" : yellow,
+                "Q" : blue, "R" : grey, "S" : light_blue, "T" : dark_blue,
+                "U" : light_blue, "V" : orange, "W" : light_blue},
+
+        "./data/gameboards/Rushhour9x9_6.csv" : { "X" : red, "A" : blue, "B" : light_blue,
+            "C" : dark_yellow, "D" : dark_blue, "E" : orange,
+            "F" : blue, "G" : light_blue, "H" : dark_blue,
+            "I" : dark_blue, "J" : orange, "K" : light_blue, "L" : grey,
+            "M" : blue, "N" : yellow, "O" : light_blue, "P" : orange,
+            "Q" : orange, "R" : dark_blue, "S" : yellow, "T" : blue,
+            "U" : dark_blue, "V" : purple, "W" : purple, "Y" : dark_yellow, "Z" : grey}
+        }
+        
+    colormap = colormaps[csv]
+
     animationframes = []
 
-    fig = plt.figure("animation",dpi=100)
+    fig = plt.figure("animation", dpi=100, figsize=(5.0,5.0))
     animationframes.append((plt.imshow(make_animation_frame(board.board, size, colormap)),))
 
     for move in moves:
@@ -189,11 +236,11 @@ def make_animation(moves, size, csv):
     plt.axis("off")
     im_animation = animation.ArtistAnimation(fig, animationframes, interval=500, repeat_delay=1000, blit=True)
     im_animation.save(("animation.gif"), writer="Pillow") 
-    plt.show()
+    #plt.show()
         
 
 def make_animation_frame(board, size, colormap):
-    image = np.zeros((6,6,3))
+    image = np.zeros((size,size,3))
     for i in range(size):
         for j in range(size):
             value = board[i][j]
