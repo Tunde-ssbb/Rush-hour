3e955d52445bc052ae923a1ef292b914be79e41e
Hannebr <hannebrouwer@gmail.com>
2021-01-12 10:53:26 +0100

    random algorithm is working i think...

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -1,21 +1,25 @@
 from code.classes.board import Board
 import random
 
-def random(moves):
+def random_algorithm(moves):
     data = "./data/gameboards/Rushhour6x6_1.csv"
     game = Board(6, data)
 
     game.load_board()
+    game.draw_board()
 
     current_move = 0
 
     while current_move < moves:
         possible_moves = game.find_moves()
-        car = random.choice(possible_moves.keys())
+        car = random.choice(list(possible_moves.keys()))
         step = random.randrange(possible_moves[car][0], possible_moves[car][1])
-        game.move(car, step)
-        game.log_move(car, step)
-
+        if step != 0:
+            game.move(car, step)
+            game.log_move(car, step)
+            game.draw_board()
+            current_move += 1
+        
         if game.won():
             break
 
--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 from code.classes.board import Board
 from code.classes.car import Car
 from code.classes.board import make_animation
+from code.algorithms.random import random_algorithm
+import random
 
-board_number = "4"
-board_sizes = { "1": 6, "2": 6, "3": 6,
-                "4": 9, "5": 9, "6": 9,
-                "7": 12}
+random_algorithm(10)
 
-data = f"./data/gameboards/Rushhour{board_sizes[board_number]}x{board_sizes[board_number]}_{board_number}.csv"
-board = Board(board_sizes[board_number],data)
+# board_number = "4"
+# board_sizes = { "1": 6, "2": 6, "3": 6,
+#                 "4": 9, "5": 9, "6": 9,
+#                 "7": 12}
 
-board.load_board()
-board.draw_board()
+# data = f"./data/gameboards/Rushhour{board_sizes[board_number]}x{board_sizes[board_number]}_{board_number}.csv"
+# board = Board(board_sizes[board_number],data)
 
-while True:
-    move = input("Car:")
+# board.load_board()
+# board.draw_board()
 
-    if move == "q":
-        break
-    step = int(input("Step: "))
-    if move not in board.cars.keys():
-        print("Invalid car")
-    elif board.validate_move(move, step):
-        board.move(move, step)
-        board.log_move(move, step)
-        board.draw_board()
-        print(board.find_moves())
-        if board.won():
-            print("Game was won")
-            break
-    else:
-        print("Invalid move")
+# while True:
+#     move = input("Car:")
+
+#     if move == "q":
+#         break
+#     step = int(input("Step: "))
+#     if move not in board.cars.keys():
+#         print("Invalid car")
+#     elif board.validate_move(move, step):
+#         board.move(move, step)
+#         board.log_move(move, step)
+#         board.draw_board()
+#         print(board.find_moves())
+#         if board.won():
+#             print("Game was won")
+#             break
+#     else:
+#         print("Invalid move")
     
-make_animation(board.moves, board.size, data)
-#board.save_log()
-print("Game ended")
+# make_animation(board.moves, board.size, data)
+# #board.save_log()
+# print("Game ended")
      
 
