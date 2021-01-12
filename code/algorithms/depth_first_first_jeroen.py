5158e69de4b6309bdf2a74d64b9048c60acb0cc3
Jeroen van der Borgh <jeroen.vanderborgh@gmail.com>
2021-01-12 16:36:09 +0100

    depth_first by Jeroen

--- code/algorithms/depth_first_first_jeroen.py
+++ code/algorithms/depth_first_first_jeroen.py
@@ -2,30 +2,33 @@ from code.classes.board import Board
 
 def depth_first_algorithm(game):
     if game.won():
-        game.working_solution_movesets.append(game.moves)
+        moves = game.moves[:]
+        #print(f"Winning moves : { game.moves }")
+        if len(moves) < len(game.shortest_solution_movesets) or game.shortest_solution_movesets == []:
+            game.log_shortest_solution_movesets(moves)
 
-    if game.give_hash() not in game.archive:
-        game.draw_board()
+
+    elif game.give_hash() not in game.archive:
+        #game.draw_board()
         current_board_state = game.give_hash() 
         game.archive_board()
-        print(game.archive)
+        #print(game.archive)
         possible_moves = game.find_moves()
         for car in possible_moves.keys():
             possible_steps = list(range(possible_moves[car][0], possible_moves[car][1]+1))
             if 0 in possible_steps:
                 possible_steps.remove(0)
-            print(f"possible_steps for {car}: {possible_steps}")
+            #print(f"possible_steps for {car}: {possible_steps}")
 
             for step in possible_steps:
-                print(f"{car} does {step}")
+                #print(f"{car} does {step}")
                 game.move(car, step)
                 game.moves.append([car,step])
-                print(F"moves taken : {game.moves}")
+                #print(F"moves taken : {game.moves}")
                 depth_first_algorithm(game)
                 game.step_back()
                 game.load_board_from_hash(current_board_state)
     else:
-        print("repeated state")
-
-
+        pass
+        #print("repeated state")
 
