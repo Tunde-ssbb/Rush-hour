fbabf87f39498c40c674206fbb4b7088da72de18
Jeroen van der Borgh <jeroen.vanderborgh@gmail.com>
2021-01-13 14:22:07 +0100

    function remove_useless_moves

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 from code.classes.board import Board
 from code.classes.car import Car
-from code.classes.board import make_animation
+from code.classes.board import make_animation, save_log
 from code.algorithms.random import random_algorithm
+from code.algorithms.depth_first_first_jeroen import depth_first_algorithm, check_solution, depth_first_main, remove_useless_moves
 import random
 
-random_algorithm(1000)
+#random_algorithm(20)
 
-# board_number = "4"
-# board_sizes = { "1": 6, "2": 6, "3": 6,
-#                 "4": 9, "5": 9, "6": 9,
-#                 "7": 12}
+board_number = "1"
+board_sizes = { "1": 6, "2": 6, "3": 6,
+                 "4": 9, "5": 9, "6": 9,
+                 "7": 12, "test" : 4}
 
-# data = f"./data/gameboards/Rushhour{board_sizes[board_number]}x{board_sizes[board_number]}_{board_number}.csv"
-# board = Board(board_sizes[board_number],data)
+data = f"./data/gameboards/Rushhour{board_sizes[board_number]}x{board_sizes[board_number]}_{board_number}.csv"
+#game = Board(board_sizes[board_number],data)
+#game.draw_board()
 
-# board.load_board()
-# board.draw_board()
+number_of_attempts = 10
+max_moves = 60
+size = board_sizes[board_number]
+solutions = depth_first_main(number_of_attempts, max_moves, size, data, True)
+#solutions = [[['A', -1], ['C', -1], ['G', 1], ['L', 1], ['B', -1], ['J', -1], ['B', 1], ['A', 1], ['I', -2], ['A', -1], ['H', 1], ['A', 1], ['E', -1], ['D', -1], ['A', -1], ['G', 1], ['D', 1], ['L', 1], ['J', -2], ['D', -1], ['E', -2], ['D', 1], ['H', -1], ['D', -1], ['I', 1], ['I', 1], ['I', 1], ['H', 1], ['E', 1], ['E', 1], ['J', 1], ['J', 1], ['J', 1], ['L', -2], ['J', -1], ['F', 1], ['X', 1], ['F', -1], ['J', 1], ['F', 1], ['E', -1], ['K', 2], ['E', -1], ['F', 1], ['X', 1], ['K', -2], ['X', 1], ['K', 2], ['G', -1], ['K', -2], ['B', -1], ['F', -2], ['I', 1], ['X', 1]]]
+#print(solutions[0])
+
+if  len(solutions) > 0:
+    short_solutions = []
+    solution_number = 1
+    for solution in solutions:
+        short_solution = solution
+        old_length = len(short_solution)
+        new_length = old_length - 1
+        while old_length > new_length:
+            #print(f"solution_number {solution_number} : length {len(short_solution)}")
+            old_length = len(short_solution)
+            short_solution = remove_useless_moves(short_solution, size, data)
+            new_length = len(short_solution)
+            #print(f"solution_number {solution_number} : to length {len(short_solution)}")
+        
+        #make_animation(short_solution, size, data, str(solution_number))
+        #save_log(short_solution, str(solution_number))
+        solution_number += 1
+        print(f"solution_number {solution_number} length : {len(short_solution)}")
+        short_solutions.append(short_solution)
+
+        
+#print(short_solutions[0])
+
 
 # while True:
 #     move = input("Car:")
