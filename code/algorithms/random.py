428e3cb07412de516c5f87c0b602a5a179675679
Hannebr <hannebrouwer@gmail.com>
2021-01-14 10:57:47 +0100

    turned random algorithm into a class

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -1,33 +1,30 @@
 from code.classes.board import Board, make_animation
 import random
 
-def random_algorithm(game):
-    """ algorithm that performs a random move out of the possible moves on the board
-        it repeats this until the game is won and saves the moves in a csv-file
-        takes a game of type Board object as input """
-    
-    game.load_board()
+class Random_algorithm:
 
-    # repeat until game is won
-    while True:
-        # find possible moves and select random car to move
-        possible_moves = game.find_moves()
-        car = random.choice(list(possible_moves.keys()))
+    def __init__(self, game):
+        self.game = game
+        self.game.load_board()
 
-        # select range of car, excluding 0, and select random step out of range
-        possible_steps = list(range(possible_moves[car][0], possible_moves[car][1]+1))
-        if 0 in possible_steps:
-            possible_steps.remove(0)
-        step = random.choice(possible_steps)
-        
-        # move car and log move
-        game.move(car, step)
-        game.log_move(car, step)
-        
-        # break out of loop if game is won
-        if game.won():
-            break
+    def run(self):
+        # repeat until game is won
+        while not self.game.won():
+            
+            # find possible moves and select random car to move
+            possible_moves = self.game.find_moves()
+            car = random.choice(list(possible_moves.keys()))
 
-    # save logged moves and create animation of moves
-    game.save_log()
-    # make_animation(game.moves, game.size, data)
+            # select range of car, excluding 0, and select random step out of range
+            possible_steps = list(range(possible_moves[car][0], possible_moves[car][1]+1))
+            if 0 in possible_steps:
+                possible_steps.remove(0)
+            step = random.choice(possible_steps)
+            
+            # move car and log move
+            self.game.move(car, step)
+            self.game.log_move(car, step)
+
+        # save logged moves and create animation of moves
+        self.game.save_log()
+        # make_animation(game.moves, game.size, data)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from code.classes.board import Board
 from code.classes.car import Car
 from code.classes.board import make_animation, save_log
-from code.algorithms.random import random_algorithm
+from code.algorithms.random import Random_algorithm
 from code.algorithms.depth_first_first_jeroen import depth_first_algorithm, check_solution, depth_first_main, remove_useless_moves
 from code.algorithms.breadth_first_Tünde import breadth_first_algorithm
 import random
@@ -9,91 +9,93 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-# dictionary of board numbers with corresponding sizes
-board_sizes = { "1": 6, "2": 6, "3": 6,
-                 "4": 9, "5": 9, "6": 9,
-                 "7": 12, "test" : 4}
+if __name__ == "__main__":
+    # dictionary of board numbers with corresponding sizes
+    board_sizes = { "1": 6, "2": 6, "3": 6,
+                    "4": 9, "5": 9, "6": 9,
+                    "7": 12, "test" : 4}
 
-# check if command line argument input is valid
-if len(sys.argv) != 3:
-    print("Invalid input. Use format: main.py <board number> <algorithm>")
-    sys.exit(0)
+    # check if command line argument input is valid
+    if len(sys.argv) != 3:
+        print("Invalid input. Use format: main.py <board number> <algorithm>")
+        sys.exit(0)
 
-board_number = sys.argv[1]
-algorithm = sys.argv[2]
+    board_number = sys.argv[1]
+    algorithm = sys.argv[2]
 
-# check if board number is valid
-if board_number not in board_sizes:
-    print("Invalid board number. Choose a board number from 1 to 7")
-    sys.exit(0)
+    # check if board number is valid
+    if board_number not in board_sizes:
+        print("Invalid board number. Choose a board number from 1 to 7")
+        sys.exit(0)
 
-size = board_sizes[board_number]
-data = f"./data/gameboards/Rushhour{board_sizes[board_number]}x{board_sizes[board_number]}_{board_number}.csv"
-
-
-# --------------------------- Random algorithm --------------------------
-if algorithm == "random":
-    # create board object and run algorithm
-    game = Board(size, data)
-    random_algorithm(game)
-    print(f"Number of moves: {len(game.moves)}")
+    size = board_sizes[board_number]
+    data = f"./data/gameboards/Rushhour{board_sizes[board_number]}x{board_sizes[board_number]}_{board_number}.csv"
 
 
-# --------------------------- depth algorithm --------------------------
-elif algorithm == "depth_first":
-    number_of_attempts = int(input("Number of attempts: "))
-    max_moves = int(input("Maximum number of moves: "))
-    solutions = depth_first_main(number_of_attempts, max_moves, size, data, True)
+    # --------------------------- Random algorithm --------------------------
+    if algorithm == "random":
+        # create board object and run algorithm
+        game = Board(size, data)
+        random = Random_algorithm(game)
+        random.run()
+        print(f"Number of moves: {len(random.game.moves)}")
 
-    if  len(solutions) > 0:
-        short_solutions = []
-        solution_number = 1
-        for solution in solutions:
-            short_solution = solution
-            old_length = len(short_solution)
-            new_length = old_length - 1
-            while old_length > new_length:
-                #print(f"solution_number {solution_number} : length {len(short_solution)}")
+
+    # --------------------------- depth algorithm --------------------------
+    elif algorithm == "depth_first":
+        number_of_attempts = int(input("Number of attempts: "))
+        max_moves = int(input("Maximum number of moves: "))
+        solutions = depth_first_main(number_of_attempts, max_moves, size, data, True)
+
+        if  len(solutions) > 0:
+            short_solutions = []
+            solution_number = 1
+            for solution in solutions:
+                short_solution = solution
                 old_length = len(short_solution)
-                short_solution = remove_useless_moves(short_solution, size, data)
-                new_length = len(short_solution)
-                #print(f"solution_number {solution_number} : to length {len(short_solution)}")
-            
-            #make_animation(short_solution, size, data, str(solution_number))
-            #save_log(short_solution, str(solution_number))
-            solution_number += 1
-            print(f"solution_number {solution_number} length : {len(short_solution)}")
-            short_solutions.append(short_solution)     
-    #print(short_solutions[0])
-    #print(sys.getrecursionlimit())
+                new_length = old_length - 1
+                while old_length > new_length:
+                    #print(f"solution_number {solution_number} : length {len(short_solution)}")
+                    old_length = len(short_solution)
+                    short_solution = remove_useless_moves(short_solution, size, data)
+                    new_length = len(short_solution)
+                    #print(f"solution_number {solution_number} : to length {len(short_solution)}")
+                
+                #make_animation(short_solution, size, data, str(solution_number))
+                #save_log(short_solution, str(solution_number))
+                solution_number += 1
+                print(f"solution_number {solution_number} length : {len(short_solution)}")
+                short_solutions.append(short_solution)     
+        #print(short_solutions[0])
+        #print(sys.getrecursionlimit())
 
 
-# --------------------------- breadth algorithm --------------------------
-elif algorithm == "breadth_first":
-    game = Board(size, data)
-    game.load_board()
-    shortest, lengths = breadth_first_algorithm(game)
-    make_animation(shortest, board_sizes[board_number], data)
-    print(len(lengths)," solutions were found.")
+    # --------------------------- breadth algorithm --------------------------
+    elif algorithm == "breadth_first":
+        game = Board(size, data)
+        game.load_board()
+        shortest, lengths = breadth_first_algorithm(game)
+        make_animation(shortest, board_sizes[board_number], data)
+        print(len(lengths)," solutions were found.")
 
 
-# ------------------------------------------------------------------------
-else:
-    print("Invalid algorithm input. Choose: random, depth_first, or breadth_first")
-    sys.exit(0)
+    # ------------------------------------------------------------------------
+    else:
+        print("Invalid algorithm input. Choose: random, depth_first, or breadth_first")
+        sys.exit(0)
 
-# print(lengths)
-# lengths = [5,7,3,9,13,12,12,9,7,7]
-# lengths = np.array(lengths)
-# range_lengths = range(np.min(lengths), np.max(lengths)) 
-# height = np.zeros(len(range_lengths))
+    # print(lengths)
+    # lengths = [5,7,3,9,13,12,12,9,7,7]
+    # lengths = np.array(lengths)
+    # range_lengths = range(np.min(lengths), np.max(lengths)) 
+    # height = np.zeros(len(range_lengths))
 
-# for i in range(len(range_lengths)):
-#     height[i] = np.count_nonzero(lengths == range_lengths[i])
+    # for i in range(len(range_lengths)):
+    #     height[i] = np.count_nonzero(lengths == range_lengths[i])
 
-# print(range_lengths, height)
-# plt.plot(np.array(range_lengths),height)
-# fig = plt.figure()
-# ax = fig.add_axes([0,0,1,1])
-# ax.bar(np.array(range_lengths), height)
-# fig.show()
+    # print(range_lengths, height)
+    # plt.plot(np.array(range_lengths),height)
+    # fig = plt.figure()
+    # ax = fig.add_axes([0,0,1,1])
+    # ax.bar(np.array(range_lengths), height)
+    # fig.show()
